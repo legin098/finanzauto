c1b06e72c21eaa2d6a74bc46ba42622eb03b15b3
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2024-02-07 13:28:17 -0500

    feat(auth-pages): add signin page

---
+++ src/auth/presenter/pages/SignIn/Presenter.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Component from './Component';
+import {PropsPresenter as Props} from './models';
+
+const Presenter = ({navigation}: Props) => {
+  return <Component />;
+};
+
+export default Presenter;
---
+++ src/auth/presenter/pages/SignIn/models.ts
@@ -0,0 +1,4 @@
+import {StackScreenProps} from '@react-navigation/stack';
+import {AuthStackParamList} from '@src/common/presenter/models';
+
+export type PropsPresenter = StackScreenProps<AuthStackParamList, 'SignIn'>;
