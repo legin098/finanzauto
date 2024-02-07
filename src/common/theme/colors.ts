94f8502f28e40ac06ae6976360e56b50425dc601
Jesus Antonio Cruz Suarez <cruzsuarezjesusantonio@gmail.com>
2024-02-07 13:29:53 -0500

    feat(common-theme): Add Theme

---
+++ src/common/theme/colors.ts
@@ -0,0 +1,16 @@
+export const COLORS = {
+  common: {
+    black: '#000000',
+    white: '#FFFFFF',
+    white_light: '#FAFAFA',
+    white_medium: '#F5F5F8',
+    gray_light: '#EDEDED',
+    gray: '#888888',
+    gray_medium: '#BEBEBE',
+    gray_black: '#61646B',
+    red: '#ff0000',
+    yellow: '#ffd200',
+  },
+  dark: {},
+  light: {},
+};
